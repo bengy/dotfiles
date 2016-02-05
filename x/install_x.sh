1ecd42bd5c62ae3f29aa397dd052608b697a3ec6
Benjamin Rau <benngiro@gmail.com>
2016-02-05 15:53:54 +0100

    X config files added

---
+++ x/install_x.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
+# ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║
+# ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║
+# ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║
+# ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
+# ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
+#
+# Author: Benjamin Rau
+# License: WTFPL
+
+## Install x config files.
+
+# Link the xinitrc file to the home directory.
+create_link $DOTFILE_DIR/x/xinitrc $HOME/.xinitrc
+
+# Load the xresource file.
+xrdb $DOTFILE_DIR/x/xresources
