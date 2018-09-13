f258653925b86910c6bba2c21bf685214c7431dc
Benjamin Rau <benjamin.rau@daimler.com>
2018-09-13 09:41:07 +0200

    Remove history

--- zsh/zshrc_scripts/aliases.zsh
+++ zsh/zshrc_scripts/aliases.zsh
@@ -8,9 +8,6 @@ alias ls='ls --color=auto'
 alias ll='ls -l --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias la='ls -la --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 
-# Open things with their default app.
-alias open='xdg-open'
-
 # Show disk usage.
 alias df='df -h'
 alias du='du -c -h'
@@ -23,4 +20,4 @@ alias back='cd $OLDPWD'
 # Very short aliases for most used commands.
 alias n="nvim"
 alias t="tmux"
-alias o='xdg-open'
+alias o="open"
